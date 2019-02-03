f8bee0f2d77c3455abc5c450247a1298b4766d5f
sorey14 <sorey14@rambler.ru>
2019-02-03 10:38:47 +0200

    add check for complex form input date to yable and text for QRCode

--- generate.php
+++ generate.php
@@ -2,16 +2,24 @@
 	session_start();
 include 'phpqrcode/qrlib.php';
 
-		if ( isset( $_POST['barcode']) && !empty($_POST['barcode'])  ) {
+	if ( isset( $_POST['barcode'] ) && ! empty( $_POST['barcode'] ) ) {
 		$code = $_POST['barcode'];
 		QRcode::png( $code, 'img/qrcode.png' );
-			foreach ( $_POST as $name => $val )
-			{
-			if(!empty($val)) $_SESSION[ $name ] = $val;
-			}
-//			 echo count( $_POST );
-			header( "location: http://DemoPDF/demo.php" );
+		$check_post=0;
+		foreach ( $_POST as $name => $val ) {
+			if ( ! empty( $val ) ) {
+				$_SESSION[ $name ] = $val;
+				$check_post +=$val;			}
+		}
+		if ($check_post==0){
+			 
+			$_SESSION['error']='true';
+			unset($_SESSION['error_']);
+			header('Location: index.php#block_notsimple ');
 			return;
+		}
+		header( "location: http://DemoPDF/demo.php" );
+		return;
 	}
 	$_SESSION['error']='true';
 	unset($_SESSION['error_']);
